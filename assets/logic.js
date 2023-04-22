05d974a84aa8bac4185fb447c74f10de204b90d5
rtm713 <rtm713@gmail.com>
2023-04-22 01:49:48 -0500

    added my code from practice-repo

---
+++ assets/logic.js
@@ -0,0 +1,60 @@
+var searchBox = document.querySelector('#searchBox');
+var searchButton = document.querySelector('#searchButton');
+var results = document.querySelector('#resultContainer');
+
+var NASA_API_URL = 'https://images-api.nasa.gov/search?q='
+
+
+searchButton.addEventListener('click', function() {
+    var userInput = searchBox.value;
+    fetchResults(userInput);
+})
+
+
+function fetchResults(input) {
+    fetch (NASA_API_URL + input)
+    .then(function (res) {
+        if (!res.ok) throw new Error('oops got an error');
+        return res.json();
+    })
+    .then(function (data) {
+        console.log('data :>>', data);
+        renderResults(data.collection.items);
+    })
+    .catch(function (error) {
+        console.error(error);
+    });
+}
+
+function renderResults(NasaData) {
+    results.textContent= "";
+    if (NasaData.length === 0) {
+        var noResults = document.createElement('h2');
+        noResults.textContent = "Sorry, nothing was found";
+        results.append(noResults);
+    }
+
+    for (i=0; i < NasaData.length; i++) {
+        var resultLinks = (NasaData[i].links[0]);
+        var resultData = (NasaData[i].data[0]);
+        var resultIMG = resultLinks.href;
+        var resultTitle = resultData.title;
+        var resultDesc = resultData.description;
+
+        var newIMG = document.createElement('img');
+        newIMG.setAttribute('src',resultIMG);
+
+        var newTitle = document.createElement('h4');
+        newTitle.textContent = resultTitle;
+
+        var newDesc = document.createElement('p');
+        newDesc.textContent = resultDesc;
+
+        var resultBox = document.createElement('div')
+        resultBox.append(newIMG);
+        resultBox.append(newTitle);
+        resultBox.append(newDesc);
+
+        results.append(resultBox);
+}
+}
